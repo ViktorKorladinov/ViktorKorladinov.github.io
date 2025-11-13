34f0419e54644c70ad8678f594cd0fecc14ddc11
Viktor Korladinov <korlavik@fel.cvut.cz>
2025-11-13 13:04:21 +0100

    admin

--- src/utils/processTalks.js
+++ src/utils/processTalks.js
@@ -1,28 +1,37 @@
-import {getCollection} from "astro:content";
-import {DateTime} from "luxon";
+import {getCollection} from 'astro:content';
+import {DateTime} from 'luxon';
 
 async function getTalks() {
-    const entries = await getCollection("talks");
-    let talks = entries.map(({data}) => {
-        const datePrague = DateTime.fromISO(`${data.datePrague}T15:00:00`, {zone: "Europe/Prague"});
-        const times = {
-            utc: datePrague.setZone("UTC").toFormat("MMM dd, HH:mm ccc"),
-            prague: datePrague.toFormat("MMM dd, HH:mm ccc"),
-            newYork: datePrague.setZone("America/New_York").toFormat("MMM dd, HH:mm ccc"),
-            shanghai: datePrague.setZone("Asia/Shanghai").toFormat("MMM dd, HH:mm ccc"),
-        };
+  const entries = await getCollection('talks');
+  let talks = entries.map(({data}) => {
+    const datePrague = DateTime.fromISO(`${data.datePrague}T15:00:00`,
+        {zone: 'Europe/Prague'});
+    const times = {
+      utc: datePrague.setZone('UTC').toFormat('MMM dd, HH:mm ccc'),
+      prague: datePrague.toFormat('MMM dd, HH:mm ccc'),
+      newYork: datePrague.setZone('America/New_York').
+          toFormat('MMM dd, HH:mm ccc'),
+      shanghai: datePrague.setZone('Asia/Shanghai').
+          toFormat('MMM dd, HH:mm ccc'),
+    };
 
-        return {
-            ...data,
-            keywords: data.keywords ? data.keywords.split(",").map(k => k.trim()) : [],
-            times,
-            date: datePrague.toUTC().toISO(),
-        };
-    });
-    talks = talks.toSorted((a, b) => b.id - a.id).toReversed()
-    const upcomingTalks = talks.filter(t => t.status === 'upcoming');
-    const pastTalks = talks.filter(t => t.status === 'past');
-    return {upcomingTalks, pastTalks};
+    return {
+      ...data,
+      keywords: data.keywords
+          ? data.keywords.split(',').map(k => k.trim())
+          : [],
+      times,
+      date: datePrague.toUTC().toISO(),
+    };
+  });
+  talks = talks.sort((a, b) => {
+    const dateA = new Date(a.datePrague);
+    const dateB = new Date(b.datePrague);
+    return dateB.getTime() - dateA.getTime(); // most recent first
+  });
+  const upcomingTalks = talks.filter(t => t.status === 'upcoming');
+  const pastTalks = talks.filter(t => t.status === 'past');
+  return {upcomingTalks, pastTalks};
 }
 
 export {getTalks};
